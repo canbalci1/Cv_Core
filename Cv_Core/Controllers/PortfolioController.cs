d8015ff05353249bb13be616098b57c42f6c9ee0
can balcı <canbalci94@hotmail.com>
2022-12-27 00:52:29 +0300

    Deneyimler & Portfolio CRUD İşlemleri

---
+++ Cv_Core/Controllers/PortfolioController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cv_Core.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Projeler Listesi";
+            ViewBag.v2 = "Deneyim";
+            ViewBag.v3 = "Projeler Listesi";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Ekleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Projeler Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {
+            ViewBag.v1 = "Ekleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Projeler Ekleme";
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetById(id);
+            portfolioManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Projeler Güncelleme";
+            var values = portfolioManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            portfolioManager.TUpdate(portfolio);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ DataAccessLayer/Migrations/20221226214621_mig_update_project_columns.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_update_project_columns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Portfolios",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Platform",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image4",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image3",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image2",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image1",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Portfolios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Platform",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image4",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image3",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image2",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image1",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
