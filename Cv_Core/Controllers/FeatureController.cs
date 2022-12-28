ba47cacdd147fc738bf4e81270ba7651b04cdcef
can balcı <canbalci94@hotmail.com>
2022-12-29 00:11:53 +0300

    Feature & About Alanları Düzenleme

--- Cv_Core/Controllers/FeatureController.cs
+++ Cv_Core/Controllers/FeatureController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cv_Core.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkan Sayfası";
+            var values = featureManager.TGetById(2);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");
         }
     }
 }
