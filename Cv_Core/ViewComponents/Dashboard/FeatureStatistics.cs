5e06adf98e982910e4d6afedb2d22d6474c3d1f2
can balcı <canbalci94@hotmail.com>
2023-01-04 00:42:47 +0300

    İstatistikleri Dashboarda Aktarma

--- Cv_Core/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Cv_Core/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,9 +7,13 @@ namespace Cv_Core.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            //var values = contactManager.TGetList();
+            ViewBag.v1 = c.Skills.Count();
+            ViewBag.v2 = c.Messages.Where(x=>x.Status==false).Count();
+            ViewBag.v3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.v4 = c.Experiences.Count();
             return View();
         }
     
